60647936193d170db4558b117a4844e11caedeaf
camilleemig <camille.emig@gmail.com>
2019-04-12 10:57:37 -0400

    Initial Commit

---
+++ CoreAnimationTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreAnimationTesting
+//
+//  Created by Camille Emig on 4/12/19.
+//  Copyright © 2019 Camille Emig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
